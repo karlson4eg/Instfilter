23384d3d9d407d0cb0ba92ed80017754112cc394
Evgenii Shatokhin <shatokhin.e@northeastern.edu>
2022-04-20 16:00:07 +0400

    first commit

---
+++ Instfilter/InstfilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstfilterApp.swift
+//  Instfilter
+//
+//  Created by Evi St on 4/20/22.
+//
+
+import SwiftUI
+
+@main
+struct InstfilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
